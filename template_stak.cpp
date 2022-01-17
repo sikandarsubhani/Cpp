65b12e6f08161e3066b3878e84b368a9862b1bba
sikandarsubhani <53407273+sikandarsubhani@users.noreply.github.com>
2022-01-18 03:50:37 +0500

    Template in class

--- template_stak.cpp
+++ template_stak.cpp
@@ -16,16 +16,16 @@ template <class Type>
 class Stack
 {
 private:
-Type st[MAX]; 
+T st[MAX]; 
 int top;
 public:
 Stack() //constructor
 { top = -1; }
 
-void push(Type var) //put number on stack
+void push(T var) //put number on stack
 { st[++top] = var; }
 
-Type pop() //take number off stack
+T pop() //take number off stack
 { return st[top--]; }
 
 };
@@ -36,17 +36,17 @@ Stack<float> s1; //s1 is object of class Stack<float>
 s1.push(1111.1F); //push 3 floats, pop 3 floats
 s1.push(2222.2F);
 s1.push(3333.3F);
-cout << “1: “ << s1.pop() << endl;
-cout << “2: “ << s1.pop() << endl;
-cout << “3: “ << s1.pop() << endl;
+cout << "1: " << s1.pop() << endl;
+cout << "2: " << s1.pop() << endl;
+cout << "3: " << s1.pop() << endl;
 
 
 Stack<long> s2; //s2 is object of class Stack<long>
 s2.push(123123123L); //push 3 longs, pop 3 longs
 s2.push(234234234L);
 s2.push(345345345L);
-cout << “1: “ << s2.pop() << endl;
-cout << “2: “ << s2.pop() << endl;
-cout << “3: “ << s2.pop() << endl;
+cout << "1: " << s2.pop() << endl;
+cout << "2: " << s2.pop() << endl;
+cout << "3: " << s2.pop() << endl;
 return 0;
 }
